af36234cbb7da0da0ec598fdda3c8cbccfc03aea
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-01 20:07:38 -0500

    1/10/2021

--- client/src/components/Menu.vue
+++ client/src/components/Menu.vue
@@ -1,54 +1,80 @@
 <template>
-  <div class= "ui secondary menu">
-      <div class="ui container">
-          <div class="left menu">
-            <router-link class="item" to="/">
-                <img class="ui small image" src="../assets/logo.png" alt="Ecommerce" />
-                <p>Categorías...</p>
-            </router-link>
-          </div>
-          <div class="right menu">
-               <router-link class="item" to="/login">
-               Iniciar Sesión
-               </router-link>
-          </div>
+  <div class="ui secondary menu">
+    <div class="ui container">
+      <div class="left menu">
+        <router-link class="item" to="/">
+          <img
+            class="ui small image"
+            src="../assets/logo.png"
+            alt="Ecommerce"
+          />
+          <p>Categorías...</p>
+        </router-link>
       </div>
+      <div class="right menu">
+        <router-link class="item" to="/login" v-if="!token">
+          Iniciar Sesión
+        </router-link>
+
+        <template v-if="token">
+          <router-link class="item" to="/orders">Pedidos</router-link>
+          <span class="ui item cart">
+            <i class="shopping cart icon"></i>
+          </span>
+          <span class="ui item logout" @click="logout">
+            <i class="sign-out icon"></i>
+          </span>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-    name: "Menu",
+import { getTokenApi, deleteTokenApi } from '../api/token';
 
+export default {
+  name: 'Menu',
+
+  setup() {
+    const token = getTokenApi();
+
+    const logout = () => {
+      deleteTokenApi();
+      location.replace('/');
+    };
+    return {
+      token,
+      logout,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.ui.menu.secondary{
-    background-color:#16202b;
-    .item{
-        color:#ffffff;
-        &:hover{
-            color:#1fa1f1;
-        }
+.ui.menu.secondary {
+  background-color: #16202b;
+  .item {
+    color: #ffffff;
+    &:hover {
+      color: #1fa1f1;
     }
-    .menu.left{
-        width: 50%;
-        .ui.image{
-            width: 70px;
-        }
+  }
+  .menu.left {
+    width: 50%;
+    .ui.image {
+      width: 70px;
     }
-    .menu.right{
-        width:50% ;
-        justify-content: flex-end;
-        .logout,
-        .cart{
-            
-            &:hover{
-                cursor: pointer;
-            }
-        }
+  }
+  .menu.right {
+    width: 50%;
+    justify-content: flex-end;
+    .logout,
+    .cart {
+      &:hover {
+        cursor: pointer;
+      }
     }
+  }
 }
-
 </style>
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -13,7 +13,7 @@
         </div>
         <div class="field">
           <input
-            type="email"
+            type="text"
             placeholder="Correo eletronico"
             v-model="formData.email"
             :class="{ error: formError.email }"
@@ -28,7 +28,11 @@
           />
         </div>
 
-        <button type="submit" class="ui button fluid primary">
+        <button
+          type="submit"
+          class="ui button fluid primary"
+          :class="{ loading }"
+        >
           Crear usuario
         </button>
       </form>
@@ -41,20 +45,28 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import * as Yup from 'yup';
 import BasicLayouts from '../layouts/BasicLayouts.vue';
 import { registerApi } from '../api/user';
+import { getTokenApi } from '../api/token';
+
 export default {
   name: 'Register',
-
   components: {
     BasicLayouts,
   },
-
   setup() {
     let formData = ref({});
     let formError = ref({});
+    let loading = ref(false);
+    const router = useRouter();
+    const token = getTokenApi();
+
+    onMounted(() => {
+      if (token) router.push('/');
+    });
 
     const schemaForm = Yup.object().shape({
       username: Yup.string().required(true),
@@ -66,12 +78,13 @@ export default {
 
     const register = async () => {
       formError.value = {};
+      loading.value = true;
 
       try {
         await schemaForm.validate(formData.value, { abortEarly: false });
         try {
           const response = await registerApi(formData.value);
-          router.log(response);
+          router.push('/login');
         } catch (error) {
           console.log(error);
         }
@@ -80,18 +93,20 @@ export default {
           formError.value[err.path] = err.message;
         });
       }
+      loading.value = false;
     };
 
     return {
       formData,
       register,
       formError,
+      loading,
     };
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoed>
 .register {
   text-align: center;
   h2 {
@@ -99,10 +114,9 @@ export default {
   }
 
   .ui.form {
-    max-width: 300px;
+    max-width: 300px !important;
     margin: 0 auto;
     margin-bottom: 10px;
-
     input.error {
       border-color: #faa;
       background-color: #ffeded;
